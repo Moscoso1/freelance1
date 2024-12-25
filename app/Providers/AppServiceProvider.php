68391fb703a6a625b3c426782dd894e77b36e842
Wilfre123 <wilfremoscoso2@gmail.com>
2024-12-25 09:23:57 +0800

    updating file

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+         // Register the custom session handler
+         $this->app->singleton('session', function ($app) {
+            // Create an instance of your custom session handler
+            $handler = new CustomDatabaseSessionHandler(
+                $app['db']->connection(), 'sessions'  // 'sessions' is the name of the session table
+            );
+
+            // Return a new instance of the SessionManager with the custom handler
+            return new SessionManager($app, $handler);
+        });
     }
 
     /**
